273060d71bedc75f4d72eb5a324edbf4117ce369
burakyapici747 <burakyapici747@gmail.com>
2024-08-26 16:28:41 +0300

    getRecentArticleService

--- my-website-fe/src/service/articleService.ts
+++ my-website-fe/src/service/articleService.ts
@@ -2,6 +2,7 @@ import {GET_REQUEST} from "@/service/genericService";
 import {SuccessfulDataResponse} from "@/service/response/response";
 import ArticleYearGroup from "@/service/model/articleYearGroup";
 import ArticleCategoryNameGroup from "@/service/model/articleCategoryNameGroup";
+import ArticleRecentByLimit from "@/service/model/articleRecentByLimit";
 
 export const getAllGroupedAndDecreasedByYear = async (): Promise<ArticleYearGroup> => {
     const result: SuccessfulDataResponse<ArticleYearGroup> = await GET_REQUEST.SINGLE_WITH_PATH_VARIABLE("/article/grouped-by-year");
@@ -10,5 +11,10 @@ export const getAllGroupedAndDecreasedByYear = async (): Promise<ArticleYearGrou
 
 export const getAllGroupedByCategoryName = async (): Promise<ArticleCategoryNameGroup> => {
     const result: SuccessfulDataResponse<ArticleCategoryNameGroup> = await GET_REQUEST.SINGLE_WITH_PATH_VARIABLE("/category/grouped-articles");
+    return result.data;
+}
+
+export const getRecentByLimit = async ():Promise<ArticleRecentByLimit> => {
+    const result: SuccessfulDataResponse<ArticleRecentByLimit> = await GET_REQUEST.SINGLE_WITH_PATH_VARIABLE("/article/recent", ["5"])
     return result.data;
 }
